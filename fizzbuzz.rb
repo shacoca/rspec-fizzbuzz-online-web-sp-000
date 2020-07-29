d7d40c59db71516221f4cacd0dc66f99e0120fbe
shawn campbell <shacoca@yahoo.com>
2020-07-29 15:36:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number % 3 == 0
+    "Fizz"
+  else
+    if number % 5 == 0
+      "Buzz"
+#    end
+  else
+    if number % 15 == 0
+      "FizzBuzz"
+#    end
+  else
+    nil
+  end
+end
